8ad8990960bb08bd8a43e16648a03fdadd0484b2
Amos <amosgathungu@gmail.com>
2017-04-29 23:32:57 -0400

    frontend updates

--- src/index.js
+++ src/index.js
@@ -1,96 +1,159 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import './index.css';
+import $ from 'jquery';
 
-class MyContacts extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            contact:[]
-
-        };
-    }
-
-    addName(event) {
-         this.setState({
-             name: event.target.value
-        })
-    }
-
-    addPhone(event) {
-         this.setState({
-             phone: event.target.value
-        })
-    }
-
-    addEmail(event) {
-         this.setState({
-             email: event.target.value
-        })
-    }
-
-    addType(event) {
-         this.setState({
-             type: event.target.value
-        })
-    }
-
-    submit(){
-        let tempArr = this.state.contact;
-        tempArr.push({name: this.state.name,
-                    phone: this.state.phone,
-                    email: this.state.email,
-                    type: this.state.type})
-
+class ContactList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      phone: '',
+      email: '',
+      type: '',
+      contactArray: []
+    };
+  }
+  componentDidMount() {
+    $.get('http://localhost:5000/api/contacts')
+      .then(contacts => {
         this.setState({
-            contact: tempArr
-        })
-    }
+          contactArray: contacts
+        });
+      });
+  }
+  changeState(stateName, event) {
+    let textInput = event.target;
+    this.setState({
+      [stateName]: textInput.value
+    });
+  }
+  submitForm(event) {
+    event.preventDefault();
+    let contact = {
+      name: this.state.name,
+      phone: this.state.phone,
+      email: this.state.email,
+      type: this.state.type
+    };
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:5000/api/contacts',
+      data: JSON.stringify(contact),
+      contentType: 'application/json'
+    })
+    .then(contact => {
+      this.state.contactArray.push(contact);
+      this.setState({
+        contactArray: this.state.contactArray,
+        name: '',
+        phone: '',
+        email: '',
+        type: ''
+      });
+    })
+    .catch(() => {
+      alert('Oops!');
+    });
+  }
+  deleteContact(id) {
+    $.ajax({
+      method: 'DELETE',
+      url: `http://localhost:5000/api/contact/${id}`
+    })
+    .then(() => {
+      let newArray = this.state.contactArray
+        .filter(function(contact) {
+          return contact.id !== id;
+        });
+      // Fancy syntax
+      // let newArray = this.state.contactArray.filter(
+      //   contact => contact.id !== id
+      // )
+      this.setState({
+        contactArray: newArray
+      });
+    })
+    .catch(() => {
+      alert('Oops!');
+    });
 
+  }
+  render() {
+    return (
+      <div className="contact-list">
+        <form onSubmit={event => this.submitForm(event)}>
+          <h3>Add Contact</h3>
+          <TextInput
+            label="Name" value={this.state.name}
+            onChange={event =>
+              this.changeState('name', event)}/>
+          <TextInput
+            label="Phone" value={this.state.phone}
+            onChange={event =>
+              this.changeState('phone', event)}/>
+          <TextInput
+            label="Email" value={this.state.email}
+            onChange={event =>
+              this.changeState('email', event)}/>
+          <SelectInput
+            label="Type" value={this.state.type}
+            onChange={event =>
+              this.changeState('type', event)}>
+            <option value="">Select one</option>
+            <option value="friend">Friend</option>
+            <option value="family">Family</option>
+            <option value="coworker">Coworker</option>
+          </SelectInput>
 
-    render() {
-
-
-        return(
-            <div className= "contacts">
-
-                <label>Name </label><br/>
-                <input type='text' value={this.state.name}
-                onChange={event => this.addName(event)} /><br/>
-                <label>Phone </label><br/>
-                <input type='text' value={this.state.phone}
-                onChange={event => this.addPhone(event)} /><br/>
-                <label>Email </label><br/>
-                <input type='text' value={this.state.email}
-                onChange={event => this.addEmail(event)} /><br/>
-
-                <label>Type </label><br/>
-
-                <select className = 'type' value={this.state.type}
-                onChange={event => this.addType(event)}>
-                <option value='co-worker'>Co-worker</option>
-                <option value='ceo'>CEO</option>
-                <option value='wife'>Spouse</option>
-                <option value='son'>Son</option>
-                <option value='son'>Neighbour</option>
-                <option value='son'>Friend</option>
-                </select><br/>
-
-                <button onClick={() => this.submit()}>Submit</button>
-
-                <ul>
-                {this.state.contact.map((item, index))}
-                {this.state.contact.map((item)=> <li>{item.name}</li>)}
-                {this.state.contact.map((item)=> <li>{item.phone}</li>)}
-                {this.state.contact.map((item)=> <li>{item.email}</li>)}
-                {this.state.contact.map((item)=> <li>{item.type}</li>)}
-                </ul>
-
-            </div>
-        );
-    }
-
+          <button className="btn btn-primary">Add</button>
+        </form>
+        <h3>Contact List</h3>
+        <ul>
+          {
+            this.state.contactArray.map((contact, idx) =>
+              <li key={idx}>
+                <h4>{contact.name} - ({contact.type})</h4>
+                {contact.phone}, {contact.email}
+                <button onClick={() => this.deleteContact(contact.id)}>Delete</button>
+              </li>
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<MyContacts/>, document.getElementById('root'));
+class TextInput extends React.Component {
+  render() {
+    return (
+      <div className="form-group">
+        <label>{this.props.label}</label>
+        <input className="form-control" type="text"
+          value={this.props.value}
+          onChange={this.props.onChange}/>
+      </div>
+    );
+  }
+}
+
+class SelectInput extends React.Component {
+  render() {
+    return (
+      <div className="form-group">
+        <label>{this.props.label}</label>
+        <select className="form-control"
+          value={this.props.value}
+          onChange={this.props.onChange}>
+          {this.props.children}
+        </select>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ContactList/>,
+  document.getElementById('root')
+);
